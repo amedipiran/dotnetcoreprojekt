20b2ce3827dc7129060b5338279c58026e0edb70
amedipiran <robertopiranamedi@gmail.com>
2024-02-21 22:01:30 +0100

    fixat delete anrop

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -62,6 +62,32 @@ private readonly ApplicationDbContext _db;
                
             return View(obj);
         }
+    public IActionResult Delete(int? id) {
+            if(id == null || id == 0){
+                return NotFound();
+            }
+
+            Category? CategoryFromDb = _db.Categories.Find(id);
+           
+
+            if (CategoryFromDb == null){
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+            public IActionResult DeletePOST(int? id) {
+           
+           Category? obj = _db.Categories.Find(id);
+            if(obj ==null) {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+
+               
+            return RedirectToAction("Index");
+        }
 
     }
 }
