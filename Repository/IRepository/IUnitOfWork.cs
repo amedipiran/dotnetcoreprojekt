30ee73c6b97cd99d07520a792dcfa5bca9bdba79
amedipiran <robertopiranamedi@gmail.com>
2024-02-26 11:43:38 +0100

    lagt till kundvagn repository

--- Repository/IRepository/IUnitOfWork.cs
+++ Repository/IRepository/IUnitOfWork.cs
@@ -10,6 +10,8 @@ namespace Projekt.Repository.IRepository {
        ICategoryRepository Category { get;}
         IProductRepository Product { get;}
         ICompanyRepository Company { get;}
+        IShoppingCartRepository ShoppingCart { get;}
+
 
        void Save(); 
     }
