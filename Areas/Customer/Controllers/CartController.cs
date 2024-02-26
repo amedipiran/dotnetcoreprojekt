6fe015a20fc2bec4d867144ec32bddef42696916
amedipiran <robertopiranamedi@gmail.com>
2024-02-26 22:52:35 +0100

    nu populeras kundvagnen

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -2,44 +2,97 @@ using System.Diagnostics;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 using Projekt.Models;
 using Projekt.Models.ViewModels;
 using Projekt.Repository;
 using Projekt.Repository.IRepository;
 
-namespace Projekt.Areas.Customer.Controllers {
+namespace Projekt.Areas.Customer.Controllers
+{
     [Area("Customer")]
     [Authorize]
-    public class CartController : Controller {
+    public class CartController : Controller
+    {
         private readonly IUnitOfWork _unitOfWork;
-        public ShoppingCartVM ShoppingCartVM {get; set;}
-        public CartController(IUnitOfWork unitOfWork) {
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
             _unitOfWork = unitOfWork;
         }
 
 
-        public IActionResult Index() {
-                var claimsIdentity = (ClaimsIdentity)User.Identity;
-    var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-    ShoppingCartVM = new () {
-        ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId == userId,includeProperties:"Product")
-    };
-    foreach(var cart in ShoppingCartVM.ShoppingCartList){
-        cart.Price = GetPriceBasedOnQuantity(cart);
-        ShoppingCartVM.OrderTotal +=(cart.Price*cart.Count);
-    }
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product")
+            };
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
             return View(ShoppingCartVM);
         }
-private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart) {
-    if (shoppingCart.Count <= 5) {
-        return shoppingCart.Product.Price ?? 0; // Ange standardvärde om null
-    } else if (shoppingCart.Count <= 10) {
-        return shoppingCart.Product.Price50 ?? 0; // Ange standardvärde om null
-    } else {
-        return shoppingCart.Product.Price100 ?? 0; // Ange standardvärde om null
+
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+
+        }
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if (cartFromDb.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 5)
+            {
+                return shoppingCart.Product.Price ?? 0;
+            }
+            else if (shoppingCart.Count <= 10)
+            {
+                return shoppingCart.Product.Price50 ?? 0;
+            }
+            else
+            {
+                return shoppingCart.Product.Price100 ?? 0;
+            }
+        }
+
+
     }
 }
-
-
-    }}
--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -6,64 +6,74 @@ using Projekt.Models;
 using Projekt.Repository;
 using Projekt.Repository.IRepository;
 
-namespace Projekt.Areas.Customer.Controllers {
+namespace Projekt.Areas.Customer.Controllers
+{
     [Area("Customer")]
-    public class HomeController : Controller {
+    public class HomeController : Controller
+    {
         private readonly IUnitOfWork _unitOfWork;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork) {
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
             _logger = logger;
             _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
             return View(productList);
         }
 
-        public IActionResult Details(int id) {
-            ShoppingCart cart = new() {
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cart = new()
+            {
                 Product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties: "Category"),
                 Count = 1,
                 ProductId = id,
             };
             return View(cart);
         }
-[HttpPost]
-[Authorize]
-public IActionResult Details(ShoppingCart shoppingCart) 
-{
-    var claimsIdentity = (ClaimsIdentity)User.Identity;
-    var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-    shoppingCart.ApplicationUserId= userId;
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
 
-    ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.Product.Id == shoppingCart.Id);
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.Product.Id == shoppingCart.Id);
 
-    if (cartFromDb != null) {
-        //shopping cart exists
-        cartFromDb.Count += shoppingCart.Count;
-       _unitOfWork.ShoppingCart.Update(cartFromDb);
-        _unitOfWork.Save();
-    }
-    else {
-        //add cart record
-        _unitOfWork.ShoppingCart.Add(shoppingCart);
-        _unitOfWork.Save();
-    }
-    TempData["success"] = "Kundvagnen uppdaterades.";
+            if (cartFromDb != null)
+            {
+                //shopping cart exists
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
+            }
+            else
+            {
+                //add cart record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+            }
+            TempData["success"] = "Kundvagnen uppdaterades.";
 
-    return RedirectToAction(nameof(Index));
-}
+            return RedirectToAction(nameof(Index));
+        }
 
 
 
-        public IActionResult Privacy() {
+        public IActionResult Privacy()
+        {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
