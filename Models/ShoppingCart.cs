420c9e38f136ccf6a36e4c6bbacf63f6c043acb1
amedipiran <robertopiranamedi@gmail.com>
2024-02-26 11:39:37 +0100

    lagt till en ny modell för kundvagn och migrerat

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Projekt.Models {
+    public class ShoppingCart  {
+        public int Id {get; set;}
+        public int ProductId {get; set;}
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product {get; set;}
+        [Range(1,100, ErrorMessage = "Vänligen välj ett antal mellan 1 och 100.")]
+        public int Count {get; set;}
+        public string ApplicationUserId {get; set;}
+          [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser {get; set;}
+
+
+
+    }
+}
