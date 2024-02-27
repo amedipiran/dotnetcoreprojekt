9ca22d728a1c9dfa2218eecece01008621aa270c
amedipiran <robertopiranamedi@gmail.com>
2024-02-27 14:48:42 +0100

    lagt till repository för order

---
+++ Repository/OrderDetailRepository copy.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projekt.Data;
+using Projekt.Models;
+using Projekt.Repository.IRepository;
+
+namespace Projekt.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+
+            _db.OrderHeaders.Update(obj);
+
+        }
+
+    }
+
+
+}
---
+++ Repository/OrderHeaderRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projekt.Data;
+using Projekt.Models;
+using Projekt.Repository.IRepository;
+
+namespace Projekt.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+
+            _db.OrderDetails.Update(obj);
+
+        }
+
+    }
+
+
+}
