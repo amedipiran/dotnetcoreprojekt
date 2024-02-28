e6a586d606e9bbbf6988e22f5bd4d226c102b329
amedipiran <robertopiranamedi@gmail.com>
2024-02-28 10:50:22 +0100

    lagt till översiktssida och designändringar

--- Models/OrderHeader.cs
+++ Models/OrderHeader.cs
@@ -45,6 +45,8 @@ namespace Projekt.Models
 
         [Required]
         public string City { get; set; }
+        [Required]
+        public string State { get; set; }
 
         [Required]
         public string PostalCode { get; set; }
