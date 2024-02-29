fd097e6b8bf6c979e68edc2881f2b360ef66d13d
amedipiran <robertopiranamedi@gmail.com>
2024-02-29 17:49:05 +0100

    lagt till metoder för återbetalning

--- Areas/Admin/Controllers/OrderController.cs
+++ Areas/Admin/Controllers/OrderController.cs
@@ -10,18 +10,21 @@ using Projekt.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Stripe.Climate;
 using System.Security.Claims;
+using System.Security.Cryptography.Pkcs;
+using Stripe;
 
 
 
 namespace Projekt.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class OrderController : Controller
     {
 
         private readonly IUnitOfWork _unitOfWork;
         [BindProperty]
-        public OrderVM OrderVM {get; set;}
+        public OrderVM OrderVM { get; set; }
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -33,7 +36,7 @@ namespace Projekt.Areas.Admin.Controllers
 
         public IActionResult Details(int orderId)
         {
-            OrderVM  = new()
+            OrderVM = new()
             {
                 OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
                 OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
@@ -45,17 +48,19 @@ namespace Projekt.Areas.Admin.Controllers
         [Authorize(Roles = SD.Roles_Admin_Employee)]
         public IActionResult UpdateOrderDetails(int orderId)
         {
-        var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u=>u.Id==OrderVM.OrderHeader.Id);
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
             orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
             orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
             orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
             orderHeaderFromDb.City = OrderVM.OrderHeader.City;
             orderHeaderFromDb.State = OrderVM.OrderHeader.State;
             orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
-            if(!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier)){
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+            {
                 orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
             }
-            if(!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber)){
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+            {
                 orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
             }
 
@@ -64,7 +69,72 @@ namespace Projekt.Areas.Admin.Controllers
 
             TempData["Success"] = "Orderdetaljerna uppdaterades framgångsrikt!";
 
-            return RedirectToAction(nameof(Details), new {orderId= orderHeaderFromDb.Id});
+            return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = SD.Roles_Admin_Employee)]
+        public IActionResult StartProcessing()
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["Success"] = "Orderdetaljerna uppdaterades framgångsrikt!";
+
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Roles_Admin_Employee)]
+        public IActionResult ShipOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = OrderVM.OrderHeader.OrderStatus;
+            orderHeader.ShippingDate = DateTime.Now;
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+            }
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+
+
+            _unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusShipped);
+            _unitOfWork.Save();
+            TempData["Success"] = "Beställning skickad framgångsrikt!";
+
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Roles_Admin_Employee)]
+        public IActionResult CancelOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+            if(orderHeader.PaymentStatus==SD.PaymentStatusApproved) {
+                var options = new RefundCreateOptions {
+                    Reason=RefundReasons.RequestedByCustomer,
+                    PaymentIntent=orderHeader.PaymentIntentId
+                };
+
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+            } else {
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+
+            }
+            
+            _unitOfWork.Save();
+            TempData["Success"] = "Beställning avbruten framgångsrikt!";
+
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+           
+
+
         }
 
         #region API CALLS
@@ -74,14 +144,17 @@ namespace Projekt.Areas.Admin.Controllers
             IEnumerable<OrderHeader> objOrderHeaders;
 
 
-                if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee)) {
-                    objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-                } else {
-                    var claimsIdentity = (ClaimsIdentity)User.Identity;
-                    var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-                    objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u=>u.ApplicationUserId==userId, includeProperties:"ApplicationUser");
-                }
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+            }
 
             switch (status)
             {
