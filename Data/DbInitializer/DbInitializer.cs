7368509fb53aa367e5d90865aaa3106ec64f1b62
amedipiran <robertopiranamedi@gmail.com>
2024-03-01 12:28:04 +0100

    förberett för migration till azure

---
+++ Data/DbInitializer/DbInitializer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Data.DbInitializer {
+    public class DbInitializer : IDbInitializer {
+        public void Initialize() {
+            throw new NotImplementedException();
+        }
+    }
+}
