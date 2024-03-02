d44b9b266f62285f53ae409950d417594db865b9
amedipiran <robertopiranamedi@gmail.com>
2024-03-02 21:24:54 +0100

    rensat bilder

---
+++ Models/ProductImage.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Models {
+    public class ProductImage {
+        public int Id {get; set;}
+        public string ImageUrl {get; set;}
+    }
+}
